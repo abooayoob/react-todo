5a254db4929f678765abc9811c47cde4c379c74b
Mohammad Ali Khan <brorentilhammad@gmail.com>
2022-12-22 13:22:31 +0100

    add probabilities to api, turn off strictmode

--- src/api.ts
+++ src/api.ts
@@ -1,31 +1,59 @@
+const probability = {
+  good: {
+    upper: 0.95,
+    lower: 0.05,
+  },
+  medium: {
+    upper: 0.75,
+    lower: 0.25,
+  },
+  bad: {
+    upper: 0.55,
+    lower: 0.45,
+  },
+};
+
 export const dbApi = {
   todos: new Set<string>(["Do laundry", "Cook dinner"]),
   getTodos() {
     return new Promise<{ data: typeof dbApi.todos; timeoutId: number }>(
-      (resolve) => {
+      (resolve, reject) => {
         const timeoutId = window.setTimeout(() => {
-          console.log(`getTodos`, this.todos);
-          resolve({ data: this.todos, timeoutId });
+          const chance = Math.random();
+          const { upper, lower } = probability.good;
+          if (chance < upper && chance > lower) {
+            resolve({ data: this.todos, timeoutId });
+          } else {
+            reject(Error(`Error getting todos`));
+          }
         }, 300);
       }
     );
   },
   addTodo(todo: string) {
-    return new Promise<{ data: string; timeoutId: number }>((resolve) => {
-      console.log(`addTodo`, this.todos);
-      const timeoutId = window.setTimeout(() => {
-        this.todos.add(todo);
-        resolve({ data: todo, timeoutId });
-      }, 300);
-    });
+    return new Promise<{ data: string; timeoutId: number }>(
+      (resolve, reject) => {
+        const timeoutId = window.setTimeout(() => {
+          const chance = Math.random();
+          const { upper, lower } = probability.good;
+          if (chance < upper && chance > lower) {
+            this.todos.add(todo);
+            resolve({ data: todo, timeoutId });
+          } else {
+            reject(Error(`Error adding "${todo}"`));
+          }
+        }, 300);
+      }
+    );
   },
   deleteTodo(todo: string) {
     return new Promise<{ data: string; timeoutId: number }>(
       (resolve, reject) => {
-        console.log(`deleteTodo`, this.todos);
         const timeoutId = window.setTimeout(() => {
           const chance = Math.random();
-          if (chance > 0.5) {
+          const { upper, lower } = probability.bad;
+
+          if (chance < upper && chance > lower) {
             this.todos.delete(todo);
             resolve({ data: todo, timeoutId });
           } else {
